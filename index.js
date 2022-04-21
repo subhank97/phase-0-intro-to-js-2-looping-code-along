282c38265d884f504903b738b2540378a700ab1e
Subhan Khan <subhank1997@gmail.com>
2022-04-21 13:34:20 -0400

    commit tree a76b43c07f4b03334feda4950e24983088c83edb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+   
+for (let i = 0; i < names.length; i++) {   
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+       messages.push(message);
+     }
+     return messages;
+   }
+   console.log(writeCards(['Suf', 'Sid', 'Yaq'], 'birthday'));
+
+function countDown(number){
+   while (number >= 0) {
+     console.log(number--);
+   }
+}
